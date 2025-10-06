86e9eecce1339071e8e574efb3e49cb0ff9e5ade
Jasdeep Singh <jasdeep.singh@spaceconcordia.ca>
2025-10-06 02:58:40 -0400

    Create GameEngineDriver.h

---
+++ GameEngineDriver.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Function prototypes
+void testGameStates(); // Function to test Game States functionality
+
+// How to use: 1. Compile and link the .h/.cc files together to create the .exe file. 
+//             2. Run `./GameEngineDriver.exe` in the terminal to run commands.
