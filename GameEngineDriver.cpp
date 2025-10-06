3810850b30ff09dce89c549527351881396fe6a6
Jasdeep Singh <jasdeep.singh@spaceconcordia.ca>
2025-10-06 02:26:00 -0400

    Create GameEngineDriver.cpp

---
+++ GameEngineDriver.cpp
@@ -0,0 +1,16 @@
+#include "../GameEngine.h"
+using namespace std;
+
+void testGameStates() {
+    GameEngine gEngine;
+    string command;
+    cout << gEngine << endl;
+    while(gEngine.getStateString() != "End") {
+        cout << "Enter command: ";
+        cin >> command;
+        gEngine.transition(command);
+        cout << gEngine << endl;    
+    }
+
+    
+}
